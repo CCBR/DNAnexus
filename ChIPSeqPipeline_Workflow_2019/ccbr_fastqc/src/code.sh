1f735f5525722b94d0c09c575849a9e45d48bb19
Vishal Koparde Ph.D <vishal.koparde@nih.gov>
2019-04-24 13:30:45 -0400

    no extracting of fastqc zip file

--- ChIPSeqPipeline_Workflow_2019/ccbr_fastqc/src/code.sh
+++ ChIPSeqPipeline_Workflow_2019/ccbr_fastqc/src/code.sh
@@ -15,18 +15,17 @@ SAR_PID=$!
 InFq=$(dx describe "$InFq" --name)
 dx download "$InFq" -o $InFq
 
-dx-docker run -v /data/:/data nciccbr/fastqc:v0.0.1 fastqc -f fastq --extract --threads $cpus $InFq
+dx-docker run -v /data/:/data nciccbr/fastqc:v0.0.1 fastqc -f fastq --threads $cpus $InFq
 
 result_base=`echo $InFq|awk -F".fastq" '{print $1}'`
 outHtml=${result_base}_fastqc.html
-outTxt=${result_base}_fastqc.txt
-mv ${result_base}_fastqc/fastqc_data.txt $outTxt
+outZip=${result_base}_fastqc.zip
 
 OutHtml=$(dx upload /data/$outHtml --brief)
-OutTxt=$(dx upload /data/$outTxt --brief)
+OutZip=$(dx upload /data/$outZip --brief)
 
 dx-jobutil-add-output OutHtml "$OutHtml" --class=file
-dx-jobutil-add-output OutTxt "$OutTxt" --class=file
+dx-jobutil-add-output OutZip "$OutZip" --class=file
 
 kill -9 $SAR_PID
 SarTxt=$(dx upload $sarfile --brief)
