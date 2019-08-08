3aaab9d9347eff23c62b0992d5fe3628e24aced1
skchronicles <kuhnsa2@vcu.edu>
2019-08-08 17:08:13 -0400

    Changing vcf2maf.pl command

--- ExomeSeq_activeDev/ccbr_vcf2maf_array/src/ccbr_vcf2maf_array.sh
+++ ExomeSeq_activeDev/ccbr_vcf2maf_array/src/ccbr_vcf2maf_array.sh
@@ -7,19 +7,19 @@ interval_in_seconds=10
 
 main() {
 
-	# Arguments 
+	# Arguments
 	echo "Value of VCF: '${VCF[@]}'"			# Array of TN-pair VCF files
 	echo "Value of TumorID: '${TumorID[@]}'"		# Array of strings: sampleName of tumor sample in VCF
 	echo "Value of NormalID: '${NormalID[@]}'"		# Array of strings: sampleName of normal sample in VCF
 	echo "Value of GenomeName: '${GenomeName}'"		# String: hg38, hg19, mm10
 
 	# Create Output Directory for parallel uploading
-	mkdir -p $HOME/out/MAF
+	mkdir -p $HOME/out/MAF/
 
 	# Listing all files in Root directory
 	cd /
 	(>&2 echo "DEBUG: Listing all files in root")
-	(>&2 ls -larth)
+	(>&2 ls -larth /)
 	(>&2 echo "Done listing")
 
 	# Creating a /data directory and downloading the VCF files
@@ -29,7 +29,7 @@ main() {
 	# Getting resources tsv file
 	genome2resources=$(dx describe "$Genome2Resources" --name)
 	dx download "$Genome2Resources" -o $genome2resources
-	
+
 	# Printing out contents of genome2resources files
 	(>&2 echo "Value of genome2resources and its contents: $genome2resources")
 	(>&2 cat $genome2resources)
@@ -51,9 +51,9 @@ main() {
 
 
 	(>&2 echo "DEBUG: Listing all files in /data after VEP ExAC Download")
-	(>&2 ls -larth)
+	(>&2 ls -larth /data/)
 	(>&2 echo "Done listing")
-	
+
 	# Getting VEP TAR Ball: vep_tar_ball
 	VEPTAR=$(python /get_fileid.py $GenomeName 'vep_tarball' $genome2resources)
 	vep_tarball=$(dx describe "$VEPTAR" --name)
@@ -61,10 +61,10 @@ main() {
 	tar xvf $vep_tarball
 
 
-	# Mapping Reference Genome to NCBI build and Species 
+	# Mapping Reference Genome to NCBI build and Species
 	ncbi_build="unknown"
 	species="unknown"
-	
+
 	if [ $GenomeName == "hg38" ]; then
 		ncbi_build="GRCh38"
 		species="homo_sapiens"
@@ -80,7 +80,7 @@ main() {
 	(>&2 echo "Value of ncbi_build: ${ncbi_build}")
 	(>&2 echo "Value of species: ${species}")
 
-	for i in ${!VCF[@]}; do 
+	for i in ${!VCF[@]}; do
 		vcf=$(dx describe "${VCF[$i]}" --name)
 		(>&2 echo "Downloading ${vcf} to /data...")
 		dx download "${VCF[$i]}" -o $vcf
@@ -98,18 +98,18 @@ main() {
 		(>&2 echo "Value of tumor_id: ${tumor_id}")
 		(>&2 echo "Value of normal_id: ${normal_id}")
 
-		# Listing downloaded file information 
+		# Listing downloaded file information
 		(>&2 echo "DEBUG: Listing downloaded file: $vcf")
 		(>&2 ls -larth "$vcf")
 		(>&2 echo "Done listing")
 
-		cpus=`nproc`
-	
+		#cpus=`nproc`
+
 		dx-docker run -v /data/.vep:/opt/vep/.vep -v /data:/data \
 			nciccbr/ccbr_vcf2maf_1.6.17:v0.0.3 vcf2maf.pl \
 				--vep-forks 1 \
 				--input-vcf $vcf \
-				--output-maf $HOME/out/MAF/$maf \
+				--output-maf $maf \
 				--tumor-id $tumor_id \
 				--normal-id $normal_id \
 				--vep-path /opt/vep/src/ensembl-vep \
@@ -118,14 +118,13 @@ main() {
 				--ncbi-build $ncbi_build \
 				--species $species \
 				--ref-fasta /data/$vep_genome
+
+		mv "$maf" "$HOME/out/MAF/"
 	done
 
-	(>&2 echo "DEBUG: Listing all files in /data after running vcf2maf command")
-	(>&2 ls -larth /data/)
-	(>&2 echo "Done listing")
-
+	# Listing the contents on $HOME/out/MAF
 	(>&2 echo "DEBUG: Listing all files in $HOME/out/MAF after running vcf2maf command")
-	(>&2 ls -larth $HOME/out/MAF)
+	(>&2 ls -larth $HOME/out/MAF/)
 	(>&2 echo "Done listing")
 
 	# Uploading files to DNAnexus
